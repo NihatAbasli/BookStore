2f9492ed24568ffe3555c69406f3369753fec057
Amrahova <10programmer01@gmail.com>
2024-02-24 18:40:49 +0400

    Wrong

---
+++ Admin/script.js
@@ -0,0 +1,15 @@
+let hamburgerIcon = document.querySelector("#hamburgerIcon");
+let adminLeftContent = document.querySelector("#adminLeftContent");
+let adminRightContent = document.querySelector("#adminRightContent");
+let adminLeftCross = document.querySelector(".adminLeftCross");
+let responsiveSidebar = document.querySelector("#responsiveSidebar");
+
+
+hamburgerIcon.addEventListener("click", function () {
+    responsiveSidebar.classList.add("active");
+    console.log("dd");
+
+});
+
+
+
